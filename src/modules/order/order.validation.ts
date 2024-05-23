016d4f511aac58f8131709419eb55d2d7e4e089c
abdulhakim10 <s.abdulhakim10@gmail.com>
2024-05-23 22:08:05 +0800

    order interface, model, zod validation schema added

---
+++ src/modules/order/order.validation.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+// Define the Zod schema for Order
+const orderValidationSchema = z.object({
+  email: z.string().email({ message: 'Invalid email address' }),
+  productId: z.string().nonempty({ message: 'Product ID is required' }),
+  price: z.number().min(0, { message: 'Price must be a positive number' }),
+  quantity: z.number().min(1, { message: 'Quantity must be at least 1' }),
+});
+
+// Export the schema
+export default orderValidationSchema;
